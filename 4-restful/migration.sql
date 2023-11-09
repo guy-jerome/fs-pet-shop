036cd91c5ca37021bd9327f08f3bdbbe8d84d93a
Aaron Roberts <aaronjamesroberts@gmail.com>
2023-11-09 11:40:03 -0600

    set up the migration and seed files and resets restful.js to be configured for a database

---
+++ 4-restful/migration.sql
@@ -0,0 +1,6 @@
+CREATE TABLE customers (
+  id serial PRIMARY KEY,
+  firstname VARCHAR (50) NOT NULL,
+  lastname VARCHAR (50) NOT NULL,
+  email VARCHAR (255)
+);
---
+++ 4-restful/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO pets (age, kind, name) VALUES (8,'dog','Carter');
+INSERT INTO pets (age, kind, name) VALUES (11,'dog','Lucy');
+INSERT INTO pets (age, kind, name) VALUES (5,'dog','Sophie');
